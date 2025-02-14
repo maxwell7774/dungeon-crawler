da0aba5dbace3d8739a878d9b795dffe1684f373
Adam Maxwell <maxwell7774@gmail.com>
2025-02-14 00:13:23 -0700

    Prints out map

--- src/main.c
+++ src/main.c
@@ -5,7 +5,11 @@
 int main() {
     printf("Welcome to my dungeon crawler!\n");
 
-    map_t *map = generate_map(5, 10);
+    map_t *map = generate_map(50, 50);
+    if(map == NULL) {
+        return 1;
+    }
+
     print_map(map);
 
     bool is_running = false;
@@ -13,5 +17,6 @@ int main() {
 
     }
 
+    destroy_map(map);
     return 0;
 }
--- src/map.c
+++ src/map.c
@@ -22,12 +22,17 @@ map_t *generate_map(int rows, int cols) {
     for(int i = 0; i < rows; i++) {
         tile_t *row = malloc(cols * sizeof(tile_t));
         if(row == NULL) {
+            for(int j = 0; j < i; j++) {
+                free(tiles[j]);
+            }
+            free(tiles);
+            free(map);
             return NULL;
         }
         for(int j = 0; j < cols; j++) {
             if(i == 0 || i == rows-1 || j == 0 || j == cols-1) {
                 row[j].is_wall = true;
-                row[j].sprite = '0';
+                row[j].sprite = 'O';
             }
             else {
                 row[j].is_wall = false;
@@ -46,12 +51,19 @@ void print_map(map_t *map) {
     for(int i = 0; i < map->rows; i++) {
         for(int j = 0; j < map->cols; j++) {
             printf("%c", map->tiles[i][j].sprite);
+            if(j != map->cols-1) {
+                printf(" ");
+            }
         }
         printf("\n");
     }
 }
 
-void destory_map(map_t *map) {
+void destroy_map(map_t *map) {
+    for(int i = 0; i < map->rows; i++) {
+        free(map->tiles[i]);
+    }
     free(map->tiles);
     free(map);
+    printf("MAP DESTROYED");
 }
